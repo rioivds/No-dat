4da8aa6f1b84dc9261543ea6e8ac4f359cbac033
unknown <matheosalicio@gmail.com>
2023-06-12 10:58:38 -0300

    back end parte 1

---
+++ server/proa/models.py
@@ -0,0 +1,5 @@
+from email.policy import default
+from enum import auto
+from pickle import TRUE
+from tkinter import CASCADE
+from tokenize import blank_re
---
+++ server/proa/views.py
@@ -0,0 +1,11 @@
+import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+
+
+
+
