9f325f072d648b5480c003086526db4a80677cdf
agustinaguileraa <agustinelias.aguilera@gmail.com>
2023-06-16 14:24:50 -0300

    Carga alumnos: arreglada

--- server/proa/admin.py
+++ server/proa/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
-# from proa.models import Alumno,Curso
-# admin.site.register(Alumno)
-# admin.site.register(Curso)
+from django.contrib import admin
+from proa.models import Alumno,Curso
+admin.site.register(Alumno)
+admin.site.register(Curso)
