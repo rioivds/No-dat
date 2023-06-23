9c604d2264b010b869b2f46dffad770d1e6cd2a7
rioivds <118768710+rioivds@users.noreply.github.com>
2023-06-23 20:25:46 -0300

    Correcciones en la edicion de Calificaciones

--- server/proa/views_calificaciones.py
+++ server/proa/views_calificaciones.py
@@ -66,37 +66,51 @@ def guardar_calificaciones(request):
         )
     insert.save()
     calificaciones = Calificaciones.objects.all()
-    return render(request, 'calificaciones/index.html', {"mensaje": "Se insertó calificación con éxito", "calificaciones": calificaciones})
+    materias = Materia.objects.all()
+    profesores = Profesor.objects.all()
+    cursos = Curso.objects.all()
+    alumnos = Alumno.objects.all()
+    return render(request, 'calificaciones/index.html', {"mensaje": "Se insertó calificación con éxito", "calificaciones": calificaciones,"materias": materias, "cursos": cursos, "profesores": profesores, "alumnos": alumnos})
 
 
 def eliminar_calificaciones(request):
-    DNI = request.GET["DNI"]
-    delete = get_object_or_404(Calificaciones, dni=DNI)
+    id = request.GET["id"]
+    delete = get_object_or_404(Calificaciones, id=id)
     delete.delete()
     calificaciones = Calificaciones.objects.all()
-    return render(request, 'calificaciones/index.html',{ "mensaje": "Se elimino la calificacion con exito", "calificaciones": calificaciones})
+    materias = Materia.objects.all()
+    profesores = Profesor.objects.all()
+    cursos = Curso.objects.all()
+    alumnos = Alumno.objects.all()
+    return render(request, 'calificaciones/index.html', {"mensaje": "Se elimino calificación con éxito", "calificaciones": calificaciones,"materias": materias, "cursos": cursos, "profesores": profesores, "alumnos": alumnos})
 
 def editar_calificaciones(request):
-    DNI = request.GET["DNI"]
+    id = request.GET["id"]
     calificaciones = Calificaciones.objects.all()
-    calificaciones_editar = Calificaciones.objects.get(dni=DNI)
-    print("editar", calificaciones_editar.nombre)
-    return render(request, 'calificaciones/index.html', {"mensaje": "", "alumnos": calificaciones, "alumnos_edit": calificaciones_editar})
+    calificaciones_editar = Calificaciones.objects.get(id=id)
+    print("editar", calificaciones_editar.id)
+    materias = Materia.objects.all()
+    profesores = Profesor.objects.all()
+    cursos = Curso.objects.all()
+    alumnos = Alumno.objects.all()
+    return render(request, 'calificaciones/index.html', {"mensaje": "","calificaciones_edit":calificaciones_editar, "calificaciones": calificaciones,"materias": materias, "cursos": cursos, "profesores": profesores, "alumnos": alumnos})
 
 def guardar_edit(request):
-    
-    DNI = request.POST["DNI"]
-    nombre = request.POST["nombre"]
-    apellido = request.POST["apellido"]
-    email = request.POST["email"]
-    fecha_nacimiento_str = request.POST["fecha_nacimiento"]
-    fecha_nacimiento = parse_fecha(fecha_nacimiento_str)
-    repitio = request.POST.get("repitio") 
+    alumno=request.POST["alumno"]
     curso = request.POST["curso"]
-
+    materia=request.POST["materia"]
+    profesor=request.POST["profesor"]
+    fecha = request.POST["fecha"]
+    fecha_nota_str = datetime.strptime(fecha, "%m/%d/%Y").strftime("%Y-%m-%d")
+    nota = request.POST["nota"]
+    final = request.POST.get("final") == "on"  # Conversión a True si está marcado
     calificaciones = Calificaciones.objects.all()
-    Alumno.objects.filter(dni = DNI).update(nombre=nombre, apellido=apellido, email=email, dni=DNI, curso=Curso.objects.get(id=curso), fecha_nacimiento=fecha_nacimiento, repitio=repitio)
-    return render(request, 'calificaciones/index.html', {"mensaje": "se editó correctamente", "calificaciones": calificaciones})
+    materias = Materia.objects.all()
+    profesores = Profesor.objects.all()
+    cursos = Curso.objects.all()
+    alumnos = Alumno.objects.all()
+    Alumno.objects.filter(id = id).update(alumno=Alumno.objects.get(dni=alumno.dni), curso=Curso.objects.get(id=curso), materia=Materia.objects.get(id=materia.id), profesor=Profesor.objects.get(dni=profesor.dni),  fecha=fecha_nota_str, nota=nota, final=final)
+    return render(request, 'calificaciones/index.html', {"mensaje": "se editó correctamente", "calificaciones": calificaciones,"materias": materias, "cursos": cursos, "profesores": profesores, "alumnos": alumnos})
 
 
 def curso(request):
