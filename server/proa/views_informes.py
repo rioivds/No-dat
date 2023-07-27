6f8e422d17e3e6ad752bc2b24c0e5ced1b60b24b
Matheo <matheosalicio@gmail.com>
2023-07-27 00:38:33 -0300

    informes casi terminadis, error en el html

--- server/proa/views_informes.py
+++ server/proa/views_informes.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import F
 from django.db.models import Q
-from proa.models import Alumno, Curso
+from proa.models import Alumno, Curso, Materia, Calificaciones
 from django.db import connection
 from django.shortcuts import get_object_or_404
 from datetime import datetime
@@ -15,5 +15,18 @@ def index(request):
     return render(request, 'informes/index.html',{ "alumnos": alumnos})
 
 def generar_informe(request):
-    alumnos = Alumno.objects.all()
-    return render(request, 'informes/index.html',{ "alumnos": alumnos})
+    lista_notas = []
+    notass = []
+    DNI = request.GET["DNI"] 
+    alumno = Alumno.objects.get(dni=DNI)
+    materias = Materia.objects.filter(curso = alumno.curso)
+    for materia in materias:
+        notas = Calificaciones.objects.filter(alumno = alumno, materia = materia, curso = alumno.curso)
+        for aux in notas:
+            notass.append(int(aux.nota))
+        notass.sort(reverse = False)
+        notass.insert(0, materia.nombre)
+        lista_notas.append(notass)
+        print(notass)
+        notass.clear()
+    return render(request, 'informes/etapa_1.html',{ "alumno": alumno, "curso": alumno.curso.anio, "materias": materias, "lista_notas": lista_notas})
