1bd0b2653aa9d92d8d3cf0c3e630ed287131fb3b
unknown <fran.lopezbenavides@gmail.com>
2023-07-31 23:11:37 -0300

    actualizacion front graficos

--- server/proa/views_graficos_torta.py
+++ server/proa/views_graficos_torta.py
@@ -19,12 +19,12 @@ def grafico_torta_materia(request, materia_id):
     print("calificaciones",calificaciones)
     print("materia",materia_id)
     print("conteo",total_calificaciones)
-    porcentajes = {'1_3': 0, '4_6': 0, '7_8': 0, '9_10': 0}
+    porcentajes = {'1_3': 0, '4_6': 0, '7_8': 0, '9_10': 0,}
     if total_calificaciones > 0:
-        porcentajes['1_3'] = calificaciones.filter(nota__range=(1, 3)).count() / total_calificaciones * 100
-        porcentajes['4_6'] = calificaciones.filter(nota__range=(4, 6)).count() / total_calificaciones * 100
-        porcentajes['7_8'] = calificaciones.filter(nota__range=(7, 8)).count() / total_calificaciones * 100
-        porcentajes['9_10'] = calificaciones.filter(nota__range=(9, 10)).count() / total_calificaciones * 100
+        porcentajes['1_3'] = calificaciones.filter(nota__range=(1, 3)).count()
+        porcentajes['4_6'] = calificaciones.filter(nota__range=(4, 6)).count() 
+        porcentajes['7_8'] = calificaciones.filter(nota__range=(7, 8)).count() 
+        porcentajes['9_10'] = calificaciones.filter(nota__range=(9, 10)).count() 
 
     # Convierte porcentajes a un diccionario antes de pasarlo a JsonResponse
     porcentajes_dict = {
@@ -32,6 +32,7 @@ def grafico_torta_materia(request, materia_id):
         '4_6': porcentajes['4_6'],
         '7_8': porcentajes['7_8'],
         '9_10': porcentajes['9_10'],
+        'total_calificaciones': total_calificaciones,
     }
     print (porcentajes_dict)
     return JsonResponse(porcentajes_dict, safe=False)
