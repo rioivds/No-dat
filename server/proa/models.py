95442083b17c8e549ac381a79acbced0e4f31abf
tomito <107369445+tomimg@users.noreply.github.com>
2024-07-29 11:23:02 -0300

    Update models.py

--- server/proa/models.py
+++ server/proa/models.py
@@ -4,7 +4,6 @@ from pickle import TRUE
 from tkinter import CASCADE
 from tokenize import blank_re
 from django.db import models
-from .choices import cursos
 
 
 class Curso(models.Model):
@@ -12,28 +11,33 @@ class Curso(models.Model):
     anio = models.IntegerField(verbose_name='Año')
 
 class Profesor(models.Model):
-    dni = models.CharField(max_length=8, primary_key=True, verbose_name='DNI Profesor')
-    nombre = models.CharField(max_length=100, verbose_name='Nombre')
-    apellido = models.CharField(max_length=100, verbose_name='Apellido')
+    dni = models.BigIntegerField(primary_key=True, verbose_name='DNI Profesor')
+    nombre = models.CharField(max_length=32, verbose_name='Nombre')
+    apellido = models.CharField(max_length=32, verbose_name='Apellido')
     telefono = models.CharField(max_length=20, verbose_name='Telefono')
     email = models.EmailField(verbose_name='Email')
+    faceid = models.CharField(max_length=512, verbose_name='FaceID', default = '0')
+    password = models.CharField(max_length=20, verbose_name='Password', default = 'proa')
+    role = models.CharField(max_length=30, verbose_name='Role', default = 'profesor')
 
 class Materia(models.Model):
     id = models.BigAutoField(primary_key=True,verbose_name='ID Materia')
-    nombre = models.CharField(max_length=100, verbose_name='Nombre Materia')
+    nombre = models.CharField(max_length=32, verbose_name='Nombre Materia')
     horas_catedra = models.IntegerField(verbose_name='Horas Catedra')
-    horario = models.CharField(max_length=100, verbose_name='Horario')
     profesor = models.ForeignKey(Profesor, null=True, on_delete=models.CASCADE)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 
 class Alumno(models.Model):
-    dni = models.CharField(max_length=10, primary_key=True, verbose_name='DNI')
-    nombre = models.CharField(max_length=100, verbose_name='Nombre')
-    apellido = models.CharField(max_length=100, verbose_name='Apellido')
+    dni = models.BigIntegerField(primary_key=True, verbose_name='DNI Profesor')
+    nombre = models.CharField(max_length=32, verbose_name='Nombre')
+    apellido = models.CharField(max_length=32, verbose_name='Apellido')
     fecha_nacimiento = models.DateField(verbose_name='Fecha de nacimiento', blank=True, null=True)
     email = models.EmailField(verbose_name='Email')
     repitio = models.BooleanField(verbose_name='Repitio?',default=None, null=True)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, blank=True, null=True)
+    faceid = models.CharField(max_length=512, verbose_name='FaceID', default = '0')
+    password = models.CharField(max_length=20, verbose_name='Password', default = 'proa')
+    role = models.CharField(max_length=30, verbose_name='Role', default = 'profesor')
 
 class Calificaciones(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -44,7 +48,3 @@ class Calificaciones(models.Model):
     fecha = models.DateField(verbose_name='Fecha')
     nota = models.FloatField(verbose_name='Nota')
     final = models.BooleanField(verbose_name='final')
-
-
-    
-
