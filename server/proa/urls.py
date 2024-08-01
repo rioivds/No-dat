d69be6c049c61c4a296ec04a7eef11f2b1d6ed02
ProA Rio IV DS <118768710+rioivds@users.noreply.github.com>
2024-08-01 11:27:29 -0300

    fix: Materia showing "horario"

--- server/proa/urls.py
+++ server/proa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views,views_admin, views_alumnos, views_profesores, views_materia, views_calificaciones, views_informes, views_graficos_barra, views_graficos_torta, views_login
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='inicio'),
     path('adminred/', views_admin.index),
     path('alumnos/', views_alumnos.index),
     path('alumnos/nuevo2/', views_alumnos.guardar_alumnos),
