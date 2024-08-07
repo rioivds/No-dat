b581c3418075aa5e1c62f77d4cbc08bae9081084
jedihilario <hyanezbertola@gmail.com>
2024-08-07 19:18:58 -0300

    fix: ignore django settings in .gitignore

--- server/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('proa/', include('proa.urls')),
-    path('', include('proa.urls')),
-]
