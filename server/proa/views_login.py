e544bb5df9918b1a964edf7fff14126375a2d3ac
jedihilario <hyanezbertola@gmail.com>
2024-08-07 22:41:50 -0300

    fix: :art: Add "user not found" alert and defer scripts load
    Add an alert when a login fails. Defer scripts loading to improve performance and prevent errors.
    
    To-do task: Front-end team must adapt the alert style

--- server/proa/views_login.py
+++ server/proa/views_login.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, authenticate
 
 def entrar(request):
     if request.method == 'GET':
