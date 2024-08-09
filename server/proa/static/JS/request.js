256cc96d1dab05d8f3f1661e3cdb23c8c5d26bd4
jedihilario <hyanezbertola@gmail.com>
2024-08-08 23:22:43 -0300

    feat: :sparkles: Basic implementation of API request
    Implement a basic request structure to the giphy API using JS

---
+++ server/proa/static/JS/request.js
@@ -0,0 +1,20 @@
+const getData = (query, updateSource) => {
+    const API_KEY = '6iypQbKw0banGwrf4rVDWNE21L1cIShf';
+    const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=${API_KEY}&limit=1`;
+    let URLS = [];
+    console.log(query);
+
+    fetch(URL)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Something went bad with the API!: ' + res.statusText);
+            }
+            return res.json();
+        })
+        .then(data => {
+            updateSource(data);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+}
