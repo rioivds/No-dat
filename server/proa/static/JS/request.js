643e7f6c7e4fd272a0eaf5d79de650c332ca5672
jedihilario <hyanezbertola@gmail.com>
2024-08-10 15:58:59 -0300

    feat: :sparkles: Add basic version of AI request (ProAI200)

--- server/proa/static/JS/request.js
+++ server/proa/static/JS/request.js
@@ -1,18 +1,32 @@
-const getData = (query, updateSource) => {
-    const API_KEY = '6iypQbKw0banGwrf4rVDWNE21L1cIShf';
-    const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=${API_KEY}&limit=1`;
+// http://200.123.248.223:3000
+const updateAdvice = (query, updateSource) => {
+    const URL = 'http://127.0.0.1:3000';
 
     fetch(URL)
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Something went bad with the API!: ' + res.statusText);
-            }
+        .then (res => {
             return res.json();
         })
-        .then(data => {
+        .then (data => {
             updateSource(data);
         })
-        .catch(e => {
-            console.log(e);
-        })
+        .catch (e => console.error);
 }
+
+// const getData = (query, updateSource) => {
+//     const API_KEY = '6iypQbKw0banGwrf4rVDWNE21L1cIShf';
+//     const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(query)}&api_key=${API_KEY}&limit=1`;
+
+//     fetch(URL)
+//         .then(res => {
+//             if (!res.ok) {
+//                 throw new Error('Something went bad with the API!: ' + res.statusText);
+//             }
+//             return res.json();
+//         })
+//         .then(data => {
+//             updateSource(data);
+//         })
+//         .catch(e => {
+//             console.log(e);
+//         })
+// }
