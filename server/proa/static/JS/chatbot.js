5edc7b92a23e2315d610af3f7f07c66c77a391ae
ProA Rio IV DS <118768710+rioivds@users.noreply.github.com>
2024-08-14 19:32:55 -0300

    fix: Restructure chat modal to pre-working

--- server/proa/static/JS/chatbot.js
+++ server/proa/static/JS/chatbot.js
@@ -12,16 +12,16 @@ window.onclick = function(event) {
     }
 }
 
-document.getElementById('sendBtn').onclick = function() {
-    var input = document.getElementById('prompt');
-    var message = input.value;
+// document.getElementById('sendBtn').onclick = function() {
+//     const input = document.getElementById('prompt');
+//     const message = input.value;
     
-    if (message.trim() !== "") {
-        var newMessage = document.createElement('div');
-        newMessage.className = 'message sent';
-        newMessage.innerHTML = '<p>' + message + '</p>';
-        document.querySelector('.messages').appendChild(newMessage);
-        input.value = '';
-        document.querySelector('.messages').scrollTop = document.querySelector('.messages').scrollHeight;
-    }
-}
+//     if (message.trim() !== "") {
+//         const newMessage = document.createElement('div');
+//         newMessage.className = 'message sent';
+//         newMessage.innerHTML = '<p>' + message + '</p>';
+//         document.querySelector('.messages').appendChild(newMessage);
+//         input.value = '';
+//         document.querySelector('.messages').scrollTop = document.querySelector('.messages').scrollHeight;
+//     }
+// }
